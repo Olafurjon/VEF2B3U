131d54a267982b66a81c5d226cea3267da278598
Olafurjon <olafurjonb@hotmail.com>
2017-02-28 14:05:48 +0000

    Verkefni 7 klárað

---
+++ Verkefni 7/Spurningaliðir/js/main.js
@@ -0,0 +1,9 @@
+var id1 = document.getElementById("one");
+var id2 = document.getElementById("two");
+var id3 = document.getElementById("three");
+
+id2.innerHTML = id2.innerHTML.replace("Gunnar","Jakob");
+
+id1.className = "active";
+
+id3.remove()
---
+++ Verkefni 7/js/main.js
@@ -0,0 +1,55 @@
+var div = document.createElement('div');
+var h2 = document.createElement('h2');
+
+div.className = "quiz";
+document.body.appendChild(div);
+div.appendChild(h2);
+h2.innerHTML = ('Quizmaster3000');
+var ul = document.createElement('ul');
+div.appendChild(ul)
+var li = document.createElement('li');
+
+function shuffleArray(array) {
+ let m = array.length, t, i;
+ // While there remain elements to shuffle…
+ while (m) {
+ // Pick a remaining element…
+ i = Math.floor(Math.random() * m--);
+ // And swap it with the current element.
+ t = array[m];
+ array[m] = array[i];
+ array[i] = t;
+ }
+ return array;
+}
+
+function spurningamaker(spurning,moguleikar,rettsvar){
+	this.spurning = spurning;
+	this.moguleikar = shuffleArray(moguleikar);
+	this.rettsvar = rettsvar;
+}
+var spurning1 = new spurningamaker("Hver stal kökunni úr krúsinni í gær?",["Hann","Hún","Það","Þessi"],"Hann");
+var spurning2 = new spurningamaker("Er þetta forrit að virka?",["Já","Nei"],"Já");
+var spurning3 = new spurningamaker("Pabbi Bjarna á 3 syni, Ripp, Rapp og... ?", ["Jón","Rupp","Kjartan","Bjarna"],"Bjarna");
+
+
+var spurningar = [spurning1,spurning2,spurning3];
+
+function birtaSpurningu(){
+	var x = Math.round(Math.random() * + Math.abs(spurningar.length - 1))
+	var li = document.createElement('li');
+	ul.appendChild(li);
+	var p = document.createElement('p');
+	li.appendChild(p).innerHTML = spurningar[x]['spurning'];
+	for (var i = 0; i < spurningar[x]['moguleikar'].length ; i++) {
+		var li = document.createElement('li');
+			li.className = "ABCD";
+	ul.appendChild(li);
+	var p = document.createElement('p');
+	var abcd = ["A: ","B: ","C: ","D: "];
+	li.appendChild(p).innerHTML = abcd[i] + spurningar[x]['moguleikar'][i];
+	};
+	
+}
+
+birtaSpurningu()
